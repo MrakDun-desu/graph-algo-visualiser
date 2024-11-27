dfad6143dba0188e4f27d3a4f7ea49cc427235ad
MrakDun-desu <m.danco@protonmail.com>
2024-11-27 18:03:05 +0100

    Make graph editable

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+// -------------------- Global vars --------------------
+
+let statusLine = "";
+let newNodeId = 0;
+let selectedNode = null;
+
+// statuses
+const Status = {
+  addingNode: "Klikněte pro přidání uzlu",
+  removingNode: "Vyberte uzel pro odstranění",
+  addingEdge: "Vyberte dva uzly pro přidání hrany",
+  removingEdge: "Vyberte hranu pro odstranění",
+  runningArticulation: "Hledání artikulačních bodů",
+  runningBridges: "Hledání mostů",
+  runningBiconnected: "Hledání 2-souvislých komponentů"
+}
+
+// -------------------- Helper functions --------------------
+
+function setStatus(newStatus) {
+  statusLine = newStatus;
+  document.getElementById("status-line").textContent = newStatus;
+}
+
+// -------------------- General cytoscape stuff --------------------
 const cy = cytoscape({
   container: document.getElementById('cy'), // The container ID
-
-  elements: [ // List of nodes and edges
-    { data: { id: 'a' } },
-    { data: { id: 'b' } },
-    { data: { id: 'c' } },
-    { data: { source: 'a', target: 'b' } },
-    { data: { source: 'b', target: 'c' } },
-    { data: { source: 'c', target: 'a' } }
-  ],
 
   style: [ // Style for nodes and edges
     {
@@ -26,10 +42,109 @@ const cy = cytoscape({
       selector: 'edge',
       style: {
         'width': 2,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle'
+        'line-color': '#ccc'
       }
     }
   ],
+
+  layout: {
+    name: "cose"
+  }
 });
+
+cy.on("tap", function (event) {
+  if (event.target !== cy) {
+    return;
+  }
+
+  if (statusLine === Status.addingNode) {
+    cy.add({ data: { id: (newNodeId++).toString() }, position: event.position });
+    setStatus("");
+  }
+});
+
+cy.on("tap", "node", function (event) {
+  const node = event.target;
+  if (statusLine === Status.removingNode) {
+    cy.remove(node);
+    setStatus("");
+  }
+  if (statusLine === Status.addingEdge) {
+    if (selectedNode === null || selectedNode === node) {
+      selectedNode = node;
+    } else {
+      cy.add([
+        {
+          data: { source: selectedNode.id(), target: node.id() }
+        }
+      ])
+      selectedNode = null;
+      setStatus("");
+    }
+  }
+});
+
+cy.on("tap", "edge", function (event) {
+  const edge = event.target;
+  if (statusLine === Status.removingEdge) {
+    cy.remove(edge);
+    setStatus("");
+  }
+});
+
+function rerender() {
+  let layout = cy.layout({ name: "cose" });
+  layout.run();
+}
+
+// -------------------- Button event listeneres --------------------
+
+document.getElementById("upload").addEventListener("click", function () {
+  // TODO
+});
+
+document.getElementById("download").addEventListener("click", function () {
+  // TODO
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  cy.remove("*");
+  newNodeId = 0;
+});
+
+document.getElementById("layout").addEventListener("click", function () {
+  rerender(cy);
+});
+
+document.getElementById("add-node").addEventListener("click", function () {
+  setStatus(Status.addingNode);
+});
+
+document.getElementById("add-edge").addEventListener("click", function () {
+  selectedNode = null;
+  setStatus(Status.addingEdge);
+});
+
+document.getElementById("remove-node").addEventListener("click", function () {
+  setStatus(Status.removingNode);
+});
+
+document.getElementById("remove-edge").addEventListener("click", function () {
+  setStatus(Status.removingEdge);
+});
+
+document.getElementById("start-articulation").addEventListener("click", function () {
+  setStatus(Status.runningArticulation);
+  // TODO
+});
+
+document.getElementById("start-bridges").addEventListener("click", function () {
+  setStatus(Status.runningBridges);
+  // TODO
+});
+
+document.getElementById("start-biconnected").addEventListener("click", function () {
+  setStatus(Status.runningBiconnected);
+  // TODO
+});
+
